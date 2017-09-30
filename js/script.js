2b3c4d514409580e9a5d3ffa83c4ac635a874d42
Dan <daniel.munozdev@gmail.com>
2017-09-29 19:23:24 -0700

    Add click event listener to .image .clickable span

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ $('.peek-a-boo .clickable').click(function() {
   $('#bird-showing').slideToggle();
   $('#bird-hidden').slideToggle();
 });
+
+$('.image .clickable').click(function() {
+  $('#image-showing').fadeToggle();
+  $('#image-hidden').fadeToggle();
+});
